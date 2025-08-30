98f87c4acec54c83ea78236d059453539f216431
Igor Valamiel Peixoto Nunes <68130156+igorvalamiel@users.noreply.github.com>
2025-08-30 12:29:42 -0300

    getting input

--- trabalho1/testing.cpp
+++ trabalho1/testing.cpp
@@ -6,12 +6,27 @@ using namespace std;
 
 int main(){
 
-    ifstream infile("data.txt");
-    string line;
+    ifstream infile("data.txt"); //opening the data file
+    
+    int m; //number of edges
+    infile >> m;
 
-    while (getline(infile, line)){
-        cout << line << endl;
+    vector<vector<int>> edges; //vector to get the edges
+
+    //getting the edges
+    for (int i=0; i<m; i++){ 
+        int u, v;
+        infile >> u >> v;
+        edges.push_back({u, v});
     }
+
+    infile.close(); //closing the data file
+
+    //printing the vector edges
+    for (auto &edge : edges) {
+            cout << "[" << edge[0] << "," << edge[1] << "] ";
+        }
+        cout << endl;
 
     return 0;
 }
