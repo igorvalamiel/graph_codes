3be010f4b4de5faaa835ae439550cc0468df42dd
Igor Valamiel Peixoto Nunes <68130156+igorvalamiel@users.noreply.github.com>
2025-09-13 14:27:00 -0300

    grafo_1 testado

--- trabalho1/list_graph.cpp
+++ trabalho1/list_graph.cpp
@@ -352,7 +352,7 @@ struct graph {
 
 int main(){
     //opening the data file
-    ifstream infile("data.txt");
+    ifstream infile("grafo_2.txt");
 
     //getting the number of lines
     int nlines; infile >> nlines;
