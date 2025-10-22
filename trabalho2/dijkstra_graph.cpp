23b7bad3a7e4ed260191d4e7a503257ccfe5f544
Igor Valamiel Peixoto Nunes <68130156+igorvalamiel@users.noreply.github.com>
2025-10-22 00:44:19 -0300

    questao 2 ainda

--- trabalho2/dijkstra_graph.cpp
+++ trabalho2/dijkstra_graph.cpp
@@ -1174,20 +1174,20 @@ int main() {
     
     /*QUESTÃO 2*/
     float total_time_heap = 0;
-    //float total_time_vec = 0;
+    float total_time_vec = 0;
     
     int k = 40;
     for (int i = 1; i <= k; i++){
-        //float a = testL.Dijkstra_vector_time(i);
+        float a = testL.Dijkstra_vector_time(i);
         float b = testL.heap_dijkstra_time(i);
         total_time_heap += b;
-        //total_time_vec += a;
+        total_time_vec += a;
     }
 
     cout << "Dijkstra Heap\n";
     cout << "Tempo total: " << total_time_heap << " |  Media Heap: " << total_time_heap/k << '\n';
-    //cout << "Dijkstra Vector\n";
-    //cout << "Dijkstra Vec: " << total_time_vec << " |  Media Vec: " << total_time_vec/k << '\n';
+    cout << "Dijkstra Vector\n";
+    cout << "Dijkstra Vec: " << total_time_vec << " |  Media Vec: " << total_time_vec/k << '\n';
     
 
 
